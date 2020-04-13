f341f55484b7f00ca1da52a4091471facd7bdb05
oldkrak <oldkrak@ajou.ac.kr>
2020-04-13 12:51:16 +0900

    hello pa1

--- pa1.c
+++ pa1.c
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  **********************************************************************/
-
+//hello pa1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
